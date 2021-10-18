5917cb320f076f5fbbdc102de4fa0134c4387d30
esir <esirkings@gmail.com>
2021-10-18 09:45:23 +0300

    Rename project from twoll_tracker to twoops_tracker

--- contrib/docker/cmd.sh
+++ contrib/docker/cmd.sh
@@ -11,11 +11,11 @@ tail -n 0 -f /app/logs/*.log &
 echo Starting Gunicorn.
 exec gunicorn \
     --bind 0.0.0.0:8000 \
-    --workers=${TROLL_TRACKER_GUNICORN_WORKERS:-3} \
+    --workers=${TWOOPS_TRACKER_GUNICORN_WORKERS:-3} \
     --worker-class gevent \
-    --log-level=${TROLL_TRACKER_GUNICORN_LOG_LEVEL:-warning} \
-    --timeout=${TROLL_TRACKER_GUNICORN_TIMEOUT:-60} \
+    --log-level=${TWOOPS_GUNICORN_LOG_LEVEL:-warning} \
+    --timeout=${TWOOPS_GUNICORN_TIMEOUT:-60} \
     --log-file=/app/logs/gunicorn.log \
     --access-logfile=/app/logs/access.log \
-    --name trollTracker \
-    troll_tracker.wsgi:application
+    --name twoopsTracker \
+    twoops_tracker.wsgi:application
--- twoops_tracker/urls.py
+++ twoops_tracker/urls.py
@@ -1,4 +1,4 @@
-"""troll_tracker URL Configuration
+"""twoops_tracker URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
