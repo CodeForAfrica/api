6bef6add171e67a341c6a9bc1bc600372350c91a
esir <esirkings@gmail.com>
2021-10-18 16:41:41 +0300

    Apply code formatting.

--- twoopstracker/asgi.py
+++ twoopstracker/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twoopstracker.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twoopstracker.settings")
 
 application = get_asgi_application()
