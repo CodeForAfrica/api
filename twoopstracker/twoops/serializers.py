7406847a422a8b9e7db0245cdd68d6a48c562405
esir <esirkings@gmail.com>
2021-11-04 04:16:46 +0300

    Display more twitter account details on returned response of the LIST

--- twoopstracker/twoops/serializers.py
+++ twoopstracker/twoops/serializers.py
@@ -17,6 +17,25 @@ class TweetSerializer(serializers.ModelSerializer):
 
 
 class TwitterAccountListSerializer(serializers.ModelSerializer):
+    def get_accounts(self, obj):
+        accounts = obj.accounts.all()
+        data = []
+        for account in accounts:
+            data.append(
+                {
+                    "name": account.name,
+                    "account_id": account.account_id,
+                    "screen_name": account.screen_name,
+                }
+            )
+
+        return data
+
     class Meta:
         model = TwitterAccountsList
-        fields = "__all__"
+        fields = ["id", "name", "owner", "is_private", "accounts"]
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["accounts"] = self.get_accounts(instance)
+        return data
