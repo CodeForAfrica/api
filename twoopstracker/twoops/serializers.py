4723565922dd0f8c4d9bd9d20b9ce784c484244d
esir <esirkings@gmail.com>
2021-11-08 11:11:07 +0300

    include created_at in the Twitter Account List serializer

--- twoopstracker/twoops/serializers.py
+++ twoopstracker/twoops/serializers.py
@@ -33,7 +33,7 @@ class TwitterAccountListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = TwitterAccountsList
-        fields = ["id", "name", "owner", "is_private", "accounts"]
+        fields = ["id", "name", "created_at", "owner", "is_private", "accounts"]
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
