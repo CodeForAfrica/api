74f7bceb96540c40b70bc16dee20dcd2e4ccac1b
esir <esirkings@gmail.com>
2021-11-11 08:55:46 +0300

    Automatically create userprofile when a new user is created.

--- twoopstracker/authentication/apps.py
+++ twoopstracker/authentication/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AuthenticatioConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "twoopstracker.authentication"
+
+    def ready(self):
+        import twoopstracker.authentication.signals  # noqa
