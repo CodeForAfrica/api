9031d179d143fc39fcc4f169a30cf00e04e2f35d
esir <esirkings@gmail.com>
2021-11-12 13:49:39 +0300

    Fix typo in app config

--- twoopstracker/authentication/apps.py
+++ twoopstracker/authentication/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class AuthenticatioConfig(AppConfig):
+class AuthenticationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "twoopstracker.authentication"
 
