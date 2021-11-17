823b3e50c593472fd41ae1516f84cd573274f721
esir <esirkings@gmail.com>
2021-11-17 15:40:08 +0300

    Create a custom TwoopsTracker pagination.

--- twoopstracker/settings.py
+++ twoopstracker/settings.py
@@ -175,8 +175,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 20,
+    "DEFAULT_PAGINATION_CLASS": "twoopstracker.twoops.pagination.TwoopsTrackerPagination",
 }
 
 # OAuth2
