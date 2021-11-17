e1a8dd88f122ff2a6fb7b5d6598c974e4f8da192
esir <esirkings@gmail.com>
2021-11-17 13:07:02 +0300

    Return tokens as json response to the frontend

--- twoopstracker/authentication/serializers.py
+++ twoopstracker/authentication/serializers.py
@@ -3,5 +3,3 @@ from rest_framework import serializers
 
 class InputSerializer(serializers.Serializer):
     access_token = serializers.CharField(required=False)
-    error = serializers.CharField(required=False)
-    redirect_url = serializers.CharField(required=False)
--- twoopstracker/authentication/views.py
+++ twoopstracker/authentication/views.py
@@ -1,7 +1,6 @@
 import requests  # type: ignore
 from django.conf import settings
 from django.core.exceptions import ValidationError
-from django.shortcuts import redirect
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -35,7 +34,7 @@ def login(request):
     The backend will then decide if to allow this user to login or not.
     """
     access_token = validated_data.get("access_token")
-    redirect_url = validated_data.get("redirect_url", TWOOPSTRACKER_FRONTEND_LOGIN_URL)
+
     if access_token:
         # login successful from the F.E
         user_data = google_get_user_info(access_token=access_token)
@@ -47,12 +46,11 @@ def login(request):
         user, _ = get_or_create_user(**profile_data)
         if user:
             refresh = RefreshToken.for_user(user)
-            response = redirect(
-                f"{redirect_url}?access_token={str(refresh.access_token)}\
-                    &refresh_token={str(refresh)}/"
+            return Response(
+                {
+                    "access_token": str(refresh.access_token),
+                    "refresh_token": str(refresh),
+                }
             )
-            return response
-    elif validated_data.get("error"):
-        return redirect(f"{redirect_url}?error={validated_data.get('error')}")
 
     return Response(input_serializer.data)
