e547eae203bb5a77626cea8b9980bae09e37de75
esir <esirkings@gmail.com>
2021-11-17 15:10:49 +0300

    Adds a default pagination for API responses.

--- twoopstracker/settings.py
+++ twoopstracker/settings.py
@@ -174,7 +174,9 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
-    ]
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
 }
 
 # OAuth2
