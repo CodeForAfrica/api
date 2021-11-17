54fedc160b19c44965a20a1186be6aa77b11644f
esir <esirkings@gmail.com>
2021-11-17 15:29:49 +0300

    Change default page size to 20

--- twoopstracker/settings.py
+++ twoopstracker/settings.py
@@ -176,7 +176,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 100,
+    "PAGE_SIZE": 20,
 }
 
 # OAuth2
