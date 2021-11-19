3bb862ecdf3794a8ab8efb78967add71d3d77d75
esir <esirkings@gmail.com>
2021-11-19 12:47:12 +0300

    Use id_token to verify that an access_token has been generated from our F.E application

--- twoopstracker/authentication/serializers.py
+++ twoopstracker/authentication/serializers.py
@@ -3,3 +3,4 @@ from rest_framework import serializers
 
 class InputSerializer(serializers.Serializer):
     access_token = serializers.CharField(required=False)
+    id_token = serializers.CharField(required=False)
--- twoopstracker/authentication/views.py
+++ twoopstracker/authentication/views.py
@@ -1,6 +1,9 @@
 import requests  # type: ignore
-from django.core.exceptions import ValidationError
+from django.conf import settings
+from google.auth.transport import requests as google_requests
+from google.oauth2 import id_token
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -17,6 +20,18 @@ def google_get_user_info(access_token):
     return response.json()
 
 
+def verify_id_token(token):
+    try:
+        id_token.verify_oauth2_token(
+            token,
+            google_requests.Request(),
+            settings.TWOOPSTRACKER_GOOGLE_OAUTH2_CLIENT_ID,
+        )
+        return True
+    except Exception:
+        return False
+
+
 @api_view(["POST"])
 def login(request):
     input_serializer = InputSerializer(data=request.data)
@@ -30,23 +45,30 @@ def login(request):
     The backend will then decide if to allow this user to login or not.
     """
     access_token = validated_data.get("access_token")
+    id_token = validated_data.get("id_token")
 
-    if access_token:
-        # login successful from the F.E
-        user_data = google_get_user_info(access_token=access_token)
-        profile_data = {
-            "email": user_data["email"],
-            "first_name": user_data["given_name"],
-            "last_name": user_data["family_name"],
-        }
-        user, _ = get_or_create_user(**profile_data)
-        if user:
-            refresh = RefreshToken.for_user(user)
-            return Response(
-                {
-                    "access_token": str(refresh.access_token),
-                    "refresh_token": str(refresh),
-                }
-            )
+    if not id_token:
+        raise ValidationError({"error": "No id_token was provided."})
 
-    return Response(input_serializer.data)
+    if verify_id_token(id_token):
+        if access_token:
+            # login successful from the F.E
+            user_data = google_get_user_info(access_token=access_token)
+            profile_data = {
+                "email": user_data["email"],
+                "first_name": user_data["given_name"],
+                "last_name": user_data["family_name"],
+            }
+            user, _ = get_or_create_user(**profile_data)
+            if user:
+                refresh = RefreshToken.for_user(user)
+                return Response(
+                    {
+                        "access_token": str(refresh.access_token),
+                        "refresh_token": str(refresh),
+                    }
+                )
+        else:
+            raise ValidationError({"error": "No access_token was provided."})
+    else:
+        raise ValidationError({"error": "Invalid access token."})
