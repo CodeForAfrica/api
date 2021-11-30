364c61c6a4ca53b6ad77be5712eccc317d8de121
esir <esirkings@gmail.com>
2021-11-30 12:21:22 +0300

    Use accounts_list to determine message to return

--- twoopstracker/twoops/views.py
+++ twoopstracker/twoops/views.py
@@ -350,14 +350,14 @@ class FileUploadAPIView(generics.CreateAPIView):
                 twitter_accounts_list.save()
 
         return_response = {}
-        if errors:
+        if errors and account_lists:
             return_response["errors"] = errors
-            if len(errors) < total_accounts:
-                status_code = status.HTTP_207_MULTI_STATUS
-            elif len(errors) == total_accounts:
-                status_code = status.HTTP_400_BAD_REQUEST
-        else:
+            status_code = status.HTTP_207_MULTI_STATUS
+        elif account_lists:
             return_response["message"] = "Successfully uploaded"
             status_code = status.HTTP_201_CREATED
+        else:
+            return_response["errors"] = errors
+            status_code = status.HTTP_400_BAD_REQUEST
 
         return response.Response(return_response, status=status_code)
