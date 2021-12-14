e664710d3b6c6407b6e5448caf12469c9848471b
_ Kilemensi <clemence@codeforafrica.org>
2021-12-14 09:52:25 +0300

    Add Google login view

--- twoopstracker/authentication/views.py
+++ twoopstracker/authentication/views.py
@@ -1,26 +1,9 @@
-from rest_framework.decorators import api_view
-from rest_framework.exceptions import ValidationError
-from rest_framework.response import Response
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
 
-from .authenticators import GoogleAuthenticator
-from .serializers import InputSerializer
+from .providers.googlesub.views import GoogleSubOAuth2Adapter
 
 
-@api_view(["POST"])
-def login(request):
-    input_serializer = InputSerializer(data=request.data)
-    input_serializer.is_valid(raise_exception=True)
-
-    validated_data = input_serializer.validated_data
-    provider = validated_data.get("provider")
-    tokens = validated_data.get("tokens")
-    if provider == "google":
-        authenticator = GoogleAuthenticator()
-        response = authenticator.authenticate(
-            tokens.get("access_token"), tokens.get("id_token")
-        )
-        return Response(response)
-    else:
-        raise ValidationError(
-            {"error": f"Provider {provider} not currently supported."}
-        )
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleSubOAuth2Adapter
+    client_class = OAuth2Client
