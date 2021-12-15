f3f123e3f052f58ed358f43f89d5658c5fee88c2
esir <esirkings@gmail.com>
2021-12-15 16:24:21 +0300

    Remove raw search

--- twoopstracker/twoops/views.py
+++ twoopstracker/twoops/views.py
@@ -88,8 +88,6 @@ def get_search_type(search_string):
     #     return "phrase"
     # elif search_string.startswith("(") and search_string.endswith(")"):
     #     return "websearch"
-    # elif "," in search_string:
-    #     return "raw"
     # else:
     #     return search_type
     return "websearch"
@@ -257,8 +255,6 @@ class TweetsView(generics.ListAPIView):
                 tweets = tweets.filter(owner__screen_name__iexact=query[1:])
             else:
                 search_type = get_search_type(query)
-                if search_type == "raw":
-                    query = refromat_search_string(query)
                 vector = SearchVector("content")
                 if search_type:
                     search_query = SearchQuery(query, search_type=search_type)
