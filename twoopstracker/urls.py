53c0aa9ca76758e67121be5317bb4168a32a877a
_ Kilemensi <clemence@codeforafrica.org>
2022-01-07 14:54:58 +0300

    Try socialaccount reverse URL fix

--- twoopstracker/urls.py
+++ twoopstracker/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("twoopstracker.authentication.urls")),
     path("v1/", include("twoopstracker.twoops.urls")),
+
+    # NOTE(kilemensi): This is a temporary test fix for
+    # Reverse for 'socialaccount_signup' not found. 'socialaccount_signup' is not a valid view function or pattern name.
+    path('accounts/', include('allauth.urls')),
 ]
