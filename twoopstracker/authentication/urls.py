928163a143406e06df3444e35a3e97f11e767372
KhadijaMahanga <khadijamahanga@y7mail.com>
2022-01-18 17:00:04 +0300

    redirect registration/account-confirm-email

--- twoopstracker/authentication/urls.py
+++ twoopstracker/authentication/urls.py
@@ -1,8 +1,10 @@
 from dj_rest_auth.registration.views import VerifyEmailView
 from django.conf.urls import include
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from .views import GoogleLogin
+from django.conf import settings
 
 urlpatterns = [
     path(
@@ -11,6 +13,7 @@ urlpatterns = [
         name="account_email_verification_sent",
     ),
     path("google/", GoogleLogin.as_view(), name="google_login"),
+    path("registration/account-confirm-email/<str:key>/", RedirectView.as_view(url=f"{settings.TWOOPSTRACKER_CONFIRM_EMAIL_URL}/%(key)s")),
     path("registration/", include("dj_rest_auth.registration.urls")),
     path("", include("dj_rest_auth.urls")),
 ]
