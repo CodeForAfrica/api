98b046552c8e0a0550385e91f7367840bd1e724d
KhadijaMahanga <khadijamahanga@y7mail.com>
2022-01-24 13:37:25 +0300

    add custom registration serializer to save first name and last name if defined

---
+++ twoopstracker/authentication/serializers.py
@@ -0,0 +1,12 @@
+from dj_rest_auth.registration.serializers import RegisterSerializer
+from rest_framework import serializers
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+
+    def custom_signup(self, request, user):
+        user.first_name = self.validated_data.get('first_name', '')
+        user.last_name = self.validated_data.get('last_name', '')
+        user.save(update_fields=['first_name', 'last_name'])
--- twoopstracker/settings.py
+++ twoopstracker/settings.py
@@ -229,7 +229,13 @@ TWOOPSTRACKER_CONFIRM_EMAIL_URL = env.str(
     "TWOOPSTRACKER_CONFIRM_EMAIL_URL", "http://localhost:3000/api/auth/confirm-email"
 )
 
-DEFAULT_FROM_EMAIL = env.str("DEFAULT_FROM_EMAIL", "no-reply@trolltracker.investigate.africa")
+# Custom Register Serializer
+# https://dj-rest-auth.readthedocs.io/en/latest/configuration.html
+REST_AUTH_REGISTER_SERIALIZERS = {
+    'REGISTER_SERIALIZER': 'twoopstracker.authentication.serializers.CustomRegisterSerializer',
+}
+
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", "no-reply@trolltracker.investigate.africa")
 EMAIL_HOST = env("EMAIL_HOST", "")
 EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", True)
 EMAIL_PORT = env.int("EMAIL_PORT", 587)
