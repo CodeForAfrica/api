cf174f0f8a8479d0209efec3e56d5e8b8ce7824b
KhadijaMahanga <khadijamahanga@y7mail.com>
2022-01-25 10:45:50 +0300

    lint

--- twoopstracker/settings.py
+++ twoopstracker/settings.py
@@ -235,7 +235,9 @@ REST_AUTH_REGISTER_SERIALIZERS = {
     "REGISTER_SERIALIZER": "twoopstracker.authentication.serializers.CustomRegisterSerializer",
 }
 
-DEFAULT_FROM_EMAIL = env.str("DEFAULT_FROM_EMAIL", "no-reply@trolltracker.investigate.africa")
+DEFAULT_FROM_EMAIL = env.str(
+    "DEFAULT_FROM_EMAIL", "no-reply@trolltracker.investigate.africa"
+)
 EMAIL_HOST = env("EMAIL_HOST", "")
 EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", True)
 EMAIL_PORT = env.int("EMAIL_PORT", 587)
