98b87d58e8679865e2f95eafb15219f0fb1191c6
KhadijaMahanga <khadijamahanga@y7mail.com>
2022-01-27 10:23:00 +0300

    use custom password reset confirm view that redirects to frontend on get

--- twoopstracker/authentication/urls.py
+++ twoopstracker/authentication/urls.py
@@ -5,12 +5,12 @@ from django.conf.urls import include
 from django.urls import path
 from django.views.generic.base import RedirectView
 
-from .views import GoogleLogin
+from .views import GoogleLogin, CusstomPasswordResetConfirmView
 
 urlpatterns = [
     path(
-        "password/reset/confirm/<uidb64>/<token>/",
-        PasswordResetConfirmView.as_view(),
+        "password/reset/confirm/<uid>/<token>/",
+        CusstomPasswordResetConfirmView.as_view(),
         name="password_reset_confirm",
     ),
     path(
