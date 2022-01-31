e7c991647c2c4c3217d30b4790912652d54e8a58
Khadija Mahanga <khadijamahanga@y7mail.com>
2022-01-31 14:23:06 +0300

    Update twoopstracker/authentication/adapter.py

--- twoopstracker/authentication/adapter.py
+++ twoopstracker/authentication/adapter.py
@@ -6,5 +6,5 @@ class CustomAccountAdapter(DefaultAccountAdapter):
     def get_email_confirmation_url(self, request, emailconfirmation):
         """Constructs the email confirmation (activation) url"""
         frontend_url = settings.TWOOPSTRACKER_EMAIL_CONFRIMATION_URL.rstrip("/")
-        activation_url = f"{frontend_url}{emailconfirmation.key}"
+        activation_url = f"{frontend_url}/{emailconfirmation.key}"
         return activation_url
