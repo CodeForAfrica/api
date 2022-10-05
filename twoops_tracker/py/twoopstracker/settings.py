ec5df2a2ec06c65d7fd89095fb2c829093c93aee
esir <esirkings@gmail.com>
2022-10-05 17:05:54 +0300

    Remove STATIC_ROOT as an environment variable

--- twoops_tracker/py/twoopstracker/settings.py
+++ twoops_tracker/py/twoopstracker/settings.py
@@ -159,7 +159,7 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = env.str("STATIC_ROOT", BASE_DIR / "staticfiles")
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
