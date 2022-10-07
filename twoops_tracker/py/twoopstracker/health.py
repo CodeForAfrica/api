63612016e16497ea765f0a5c2a671bdb2515f981
Xavier Frankline <xf.xavierfrank@gmail.com>
2022-10-07 11:47:07 +0300

    optimize healthcheck fetch first query

--- twoops_tracker/py/twoopstracker/health.py
+++ twoops_tracker/py/twoopstracker/health.py
@@ -1,5 +1,3 @@
-import datetime
-from datetime import datetime,timezone
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -11,14 +9,9 @@ def health_check(request):
     # this can be expanded to include checks e.g
     # 1. has there been any tweets collected in the last 24 hours\
     # and if not it might indicate the system not being functional
-    '''
-    Check last created tweet within 6 hours
-    '''
-    t_now = datetime.datetime.now(timezone.utc)
     try:
-        last_created_tweet = Tweet.objects.only('created_at').last()
-        time_diff = t_now - last_created_tweet
-        if time_diff.seconds < 21600:
+        first_tweet = Tweet.objects.only('tweet_id').first()
+        if first_tweet:
             return Response(status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_503_SERVICE_UNAVAILABLE)
