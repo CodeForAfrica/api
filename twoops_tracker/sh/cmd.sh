fb891815cfbd0931183e3c28e666f2fd4e8e9f33
_ Kilemensi <clemence@codeforafrica.org>
2022-10-10 16:34:01 +0300

    Run script from current directory only

--- twoops_tracker/sh/cmd.sh
+++ twoops_tracker/sh/cmd.sh
@@ -1,32 +1,34 @@
 #!/bin/sh
-/bin/dj/pex manage migrate --noinput       # Apply database migrations
-/bin/dj/pex manage collectstatic --noinput # Collect static files
+./pex manage migrate --noinput       # Apply database migrations
+./pex manage collectstatic --noinput # Collect static files
 
 # Prepare log files and start outputting logs to stdout
-touch /app/logs/gunicorn.log
-touch /app/logs/access.log
-touch /app/logs/celery.log
+touch ./logs/gunicorn.log
+touch ./logs/access.log
+touch ./logs/celery.log
 
-tail -n 0 -f /app/logs/*.log &
+tail -n 0 -f ./logs/*.log &
 
 # Start celery worker
-/bin/dj/pex celery -A twoopstracker worker -l INFO >/dev/null 2>/app/logs/celery.log &
+echo Starting celery worker
+./pex celery -A twoopstracker worker -l INFO >/dev/null 2>./logs/celery.log &
 
 # everytime the container is restarted, the scheduler will reset
 rm -rf celerybeat-schedule
 # Start celery beat service
-/bin/dj/pex celery -A twoopstracker beat -l INFO >/dev/null 2>/app/logs/celery.log &
+echo Starting celery beat
+./pex celery -A twoopstracker beat -l INFO >/dev/null 2>./logs/celery.log &
 
 # Start Gunicorn processes
-echo Starting Gunicorn.
-exec /bin/dj/pex gunicorn \
+echo Starting gunicorn
+exec ./pex gunicorn \
 	--bind 0.0.0.0:8000 \
-	--workers="${TWOOPS_TRACKER_GUNICORN_WORKERS:-3}" \
+	--workers="${TWOOPSTRACKER_GUNICORN_WORKERS:-3}" \
 	--worker-class gevent \
-	--log-level="${TWOOPS_GUNICORN_LOG_LEVEL:-warning}" \
-	--timeout="${TWOOPS_GUNICORN_TIMEOUT:-60}" \
-	--log-file=/app/logs/gunicorn.log \
-	--access-logfile=/app/logs/access.log \
-	--name twoopsTracker \
-	"${TWOOPSTRACKER_GUNICORN_EXTRA_CONFIG:-}" \
+	--log-level="${TWOOPSTRACKER_GUNICORN_LOG_LEVEL:-warning}" \
+	--timeout="${TWOOPSTRACKER_GUNICORN_TIMEOUT:-60}" \
+	--log-file=./logs/gunicorn.log \
+	--access-logfile=./logs/access.log \
+	--name twoopstracker \
+	"${TWOOPSTRACKER_GUNICORN_EXTRA_CONFIG:---reload}" \
 	twoopstracker.wsgi:application
