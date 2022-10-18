3011193739bf06954a1639af14f08ed8c08e128a
Xavier Frankline <xf.xavierfrank@gmail.com>
2022-10-18 13:35:50 +0300

    return last tweet ID in healthcheck endpoint

--- twoops_tracker/py/twoopstracker/health.py
+++ twoops_tracker/py/twoopstracker/health.py
@@ -10,9 +10,9 @@ def health_check(request):
     # 1. has there been any tweets collected in the last 24 hours\
     # and if not it might indicate the system not being functional
     try:
-        first_tweet = Tweet.objects.only("tweet_id").first()
-        if first_tweet:
-            return Response(status=status.HTTP_200_OK)
+        last_tweet_id = Tweet.objects.values_list("tweet_id", flat=True).last()
+        if last_tweet_id is not None:
+            return Response({'tweet_id':last_tweet_id},status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_503_SERVICE_UNAVAILABLE)
     except Exception:
