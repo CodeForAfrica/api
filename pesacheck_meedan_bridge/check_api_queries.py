64c0da7bb69680555845291ba80e26827d8f6b14
Kevin Koech <koechkevin92@gmail.com>
2024-02-02 16:53:20 +0300

    Lint and use pants

--- pesacheck_meedan_bridge/check_api_queries.py
+++ pesacheck_meedan_bridge/check_api_queries.py
@@ -1,10 +1,18 @@
 import json
 
 
-def create_mutation_query(media_type=None, channel=None, set_tags=None,
-                          set_status=None, set_claim_description=None,
-                          title=None, summary=None, url=None,
-                          language=None, publish_report=False):
+def create_mutation_query(
+    media_type=None,
+    channel=None,
+    set_tags=None,
+    set_status=None,
+    set_claim_description=None,
+    title=None,
+    summary=None,
+    url=None,
+    language=None,
+    publish_report=False,
+):
     mutation_query = f"""
       mutation create {{
         createProjectMedia(input: {{
@@ -38,11 +46,11 @@ def create_mutation_query(media_type=None, channel=None, set_tags=None,
 
 
 def delete_mutation_query(id):
-    mutation_query = f'''
+    mutation_query = f"""
     mutation {{
       destroyFactCheck(input: {{
         id: "{id}"
       }}) {{ deletedId }}
     }}
-    '''
+    """
     return mutation_query
--- pesacheck_meedan_bridge/service.py
+++ pesacheck_meedan_bridge/service.py
@@ -1,15 +1,23 @@
 import re
+
 import requests
-import os
-from dotenv import load_dotenv
-from check_api_queries import create_mutation_query, delete_mutation_query
-from sentry_sdk import init, capture_exception, capture_message, set_context
+from check_api_queries import create_mutation_query
+from environs import Env
+from sentry_sdk import capture_exception, capture_message, init, set_context
 
-load_dotenv()
+env = Env()
+env.read_env()
 
+
+PESACHECK_SENTRY_DSN = env("PESACHECK_SENTRY_DSN", None)
+PESACHECK_RSS2JSON_API_KEY = env("PESACHECK_RSS2JSON_API_KEY", "")
+PESACHECK_URL = env("PESACHECK_URL", "")
+PESACHECK_CHECK_URL = env("PESACHECK_CHECK_URL", "")
+PESACHECK_TOKEN = env("PESACHECK_TOKEN", "")
+PESACHECK_CHECK_WORKSPACE_SLUG = env("PESACHECK_CHECK_WORKSPACE_SLUG", None)
 
 init(
-    dsn=os.getenv("PESACHECK_SENTRY_DSN"),
+    dsn=PESACHECK_SENTRY_DSN,
     traces_sample_rate=1.0,
     profiles_sample_rate=1.0,
 )
@@ -20,27 +28,34 @@ def log_error(code, message=""):
 
 
 def remove_html_tags(input_string):
-    return re.sub(r'<[^>]*>', '', input_string)
+    return re.sub(r"<[^>]*>", "", input_string)
 
 
 def fetch_from_pesacheck():
     url = "https://api.rss2json.com/v1/api.json"
-    rss_api_key = os.getenv("PESACHECK_RSS2JSON_API_KEY")
-    feed_url = os.getenv("PESACHECK_URL")
-    params = {'rss_url': feed_url, 'api_key': rss_api_key, 'count': 10, 'order_by': 'pubDate'}
+    feed_url = PESACHECK_URL
+    params = {
+        "rss_url": feed_url,
+        "api_key": PESACHECK_RSS2JSON_API_KEY,
+        "count": 10,
+        "order_by": "pubDate",
+    }
     response = requests.get(url, params=params, timeout=60)
     if response.status_code == 200:
-        return response.json().get('items') or []
+        return response.json().get("items") or []
     log_error(code=response.status_code, message=response.text)
     return []
 
 
 def post_to_check(query):
     try:
-        headers = {"Content-Type": "application/json",
-                   "X-Check-Token": os.getenv("PESACHECK_TOKEN"), "X-Check-Team": os.getenv("PESACHECK_CHECK_WORKSPACE_SLUG")}
+        headers = {
+            "Content-Type": "application/json",
+            "X-Check-Token": PESACHECK_TOKEN,
+            "X-Check-Team": PESACHECK_CHECK_WORKSPACE_SLUG,
+        }
         body = dict(query=query)
-        url = os.getenv("PESACHECK_CHECK_URL")
+        url = PESACHECK_CHECK_URL
         response = requests.post(url, headers=headers, json=body, timeout=60)
         if response.status_code == 200:
             return response.json()
@@ -70,8 +85,9 @@ def upload_content():
             }
             query = create_mutation_query(**input_data)
             res = post_to_check(query)
-            if (res):
+            if res:
                 success_posts.append(res)
         set_context("Successful Uploads", success_posts)
+
 
 upload_content()
